9c37f4032dc23317d5685ebab921a77ce369c260
Telecomo Dev RBG <Telecomo Dev RBG>
2017-03-21 10:03:03 -0600

    first commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    
+    use DatabaseTransactions;
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {   
+        $user= factory(\App\User::class)->create([
+            'name'=> 'Giovanny',
+            'email'=> 'g@g.com',
+        ]);
+        
+        
+        $this->actingAs($user, 'api')
+             ->visit('api/user')
+             ->see('Giovanny')
+            ->see('g@g.com');
+    }
+}
