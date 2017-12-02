3f943134eb64205dbba997448df7d1db181dffef
rabagi <programador1@telecomo.com.mx>
2017-12-02 12:06:18 -0600

    paginado paginas con dompdf ejemplo: pagina 1 de 1

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -9,14 +9,14 @@ class PdfController extends Controller
 {
     public function pdf(){
         
-        $pdf = PDF::loadView('pdf.pdf');
-        
+        $pdf = PDF::loadView('pdf.pdf', [ $htmlPDF = $this->getView('pdf.pdf', array( 'orderNumber' => 1 ))]);
+        $pdf->setOptions(['isPhpEnabled' => true]);
         return $pdf->stream('test.pdf');
 
     }
     
     
-    function getView ($file, $data=NULL) {
+    function getView($file, $data=NULL) {
         if (!empty($data)) extract($data);
         ob_start();
         if (is_file($file)) include($file);
